b8169c6bc57e20063ca2fc57f23b95b612dac331
ThansiJamal <thansijamal18@gmail.com>
2020-10-07 15:16:33 -0700

    print name with interval

---
+++ NamesTimer/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace NamesTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblNames = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNames
+            // 
+            this.lblNames.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNames.Location = new System.Drawing.Point(99, 39);
+            this.lblNames.Name = "lblNames";
+            this.lblNames.Size = new System.Drawing.Size(154, 37);
+            this.lblNames.TabIndex = 0;
+            this.lblNames.Text = "Names";
+            this.lblNames.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(140, 98);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(360, 188);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblNames);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Names Timer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNames;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ NamesTimer/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.PerformanceData;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NamesTimer
+{
+    public partial class Form1 : Form
+       
+    {
+        int count = 1;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (count <= 10)
+            {
+                if (count == 1)
+                {
+                    this.BackColor = Color.Red;
+                    lblNames.Text = "Thansi";
+                    count++;
+                }
+                else if (count == 2)
+                {
+                    this.BackColor = Color.Green;
+                    lblNames.Text = "Thalitha";
+                    count++;
+                }
+                else if (count == 3)
+                {
+                    this.BackColor = Color.Yellow;
+                    lblNames.Text = "Akhil";
+                    count++;
+                    
+                }
+
+                else if (count == 4)
+                {
+                    this.BackColor = Color.Cyan;
+                    lblNames.Text = "Rafsal";
+                    count++;
+                    
+                }
+                else if (count == 5)
+                {
+                    this.BackColor = Color.Brown;
+                    lblNames.Text = "Sali";
+                    count++;
+
+                }
+                else if (count == 6)
+                {
+                    this.BackColor = Color.Gray;
+                    lblNames.Text = "Parvin";
+                    count++;
+
+                }
+                else if (count == 7)
+                {
+                    this.BackColor = Color.Maroon;
+                    lblNames.Text = "Jubi";
+                    count++;
+
+                }
+                else if (count == 8)
+                {
+                    this.BackColor = Color.Magenta;
+                    lblNames.Text = "Jerry";
+                    count++;
+
+                }
+                else if (count == 9)
+                {
+                    this.BackColor = Color.Violet;
+                    lblNames.Text = "Jismy";
+                    count++;
+
+                }
+                else if (count == 10)
+                {
+                    this.BackColor = Color.DarkCyan;
+                    lblNames.Text = "Thareeq";
+                    count++;
+
+                }
+             
+             }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Interval = 2000;
+            //timer1.Start();
+
+            btnStart.Visible = false;
+
+
+            
+
+        }
+
+       
+    }
+}
+
+
+      
